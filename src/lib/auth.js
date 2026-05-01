147394ef6ad293558c184f8d3d036a5cdd414ebf
topu9872-cpu <topu9872@gmail.com>
2026-05-01 22:08:30 +0600

    added varcal url

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,14 +3,13 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGO_URL);
-const db = client.db('online_book_borrowing_db');
+const db = client.db("online_book_borrowing_db");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-
-    client
+    client,
   }),
-  emailAndPassword: { 
-    enabled: true, 
-  }, 
+  emailAndPassword: {
+    enabled: true,
+  },
 });
